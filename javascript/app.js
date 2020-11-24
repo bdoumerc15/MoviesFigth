acd7b7688800d256fd90ca5a17f1614911d9a192
bladimir doumerc <bdoumerc15@gmail.com>
2020-11-23 21:37:21 -0700

    se ha agregado una funcion para pedir informacion al API de OMDB y poder obtener datos (data)

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,11 @@
-console.log('Hi there!');
+const fetchData = async() => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: "3e155f78",
+            s: 'avengers'
+        }
+    })
+    return response.data;
+}
+
+console.log(fetchData());
